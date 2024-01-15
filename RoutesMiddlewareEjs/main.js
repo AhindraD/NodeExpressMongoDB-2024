19bfc867aeba4ef1a7202893b824ddd18fec1737
Ahindra D <official.ahindra@gmail.com>
2024-01-16 01:04:09 +0530

    middlewatres

--- RoutesMiddlewareEjs/main.js
+++ RoutesMiddlewareEjs/main.js
@@ -1,16 +1,30 @@
-import express from "express";
+const express = require("express");
 const app = express();
 const port = 3000;
 
 
 //MIDDLEWARES
 app.use(express.static("public"));//from Express Package
-
+//custom1
+app.use((req, res, next) => {
+    console.log("M_1");
+    next();
+})
+//custom2
+app.use((req, res, next) => {
+    console.log("M_2");
+    next();
+})
 
 
 app.get("/", (req, res) => {
-    // res.send('hello world')
-    res.sendFile("index.html", { root: __dirname })
+    console.log("simple root GET");
+    res.send('hello world');
+})
+
+app.get("/access", (req, res) => {
+    console.log("simple root GET public");
+    res.sendFile("templates/index.html", { root: __dirname })
 })
 
 
