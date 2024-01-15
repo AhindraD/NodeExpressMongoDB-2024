296f3704853f0ce048b69f38a26be2de499b971b
Ahindra D <official.ahindra@gmail.com>
2024-01-15 20:13:19 +0530

    middleWare

--- RoutesMiddlewareEjs/main.js
+++ RoutesMiddlewareEjs/main.js
@@ -2,10 +2,18 @@ import express from "express";
 const app = express();
 const port = 3000;
 
+
+//MIDDLEWARES
+app.use(express.static("public"));//from Express Package
+
+
+
 app.get("/", (req, res) => {
-    res.send('hello world')
-    // res.sendFile("index.html", { root: "__dirname" })
+    // res.send('hello world')
+    res.sendFile("index.html", { root: __dirname })
 })
+
+
 
 app.listen(port, () => {
     console.log("Express server running...")
